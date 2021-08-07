c6f5eb5878aaeb2c02deef781b8421124a2564cf
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-08-07 18:28:22 +0900

    :sparkles: Add config and publish

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Linter } from "eslint"
+
+const config: Linter.BaseConfig = {
+  env: { es2020: true, node: true /*, jest/globals: true*/ },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: "./tsconfig.json" },
+  plugins: [/*"jest",*/ "@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    /*"plugin:jest/recommended",*/
+    "prettier",
+    "plugin:import/errors",
+  ],
+  rules: {
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "sort-imports": "off",
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal"],
+        alphabetize: { order: "asc" },
+      },
+    ],
+    "@typescript-eslint/no-misused-new": "off",
+    "no-empty": "off",
+    "no-console": ["warn", { allow: ["error", "warn", "info"] }],
+  },
+  settings: {
+    "import/resolver": { node: { extensions: [".js", ".ts", ".tsx"] } },
+  },
+}
+
+module.exports = config
